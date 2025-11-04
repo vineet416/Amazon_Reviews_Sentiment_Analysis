a82f475faf87513f0059911609ddb201f468c641
Vineet Patel <vineetpatel468@gmail.com>
2025-11-04 20:29:47 +0530

    flask app API

--- app.py
+++ app.py
@@ -121,4 +121,6 @@ def predict_sentiment(text):
     return "Positive" if prediction == 1 else "Negative"
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
